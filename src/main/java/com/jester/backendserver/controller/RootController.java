f4847dd977f9aed907e90f5c801f93831583981f
mbudak21 <mbudak21@ku.edu.tr>
2025-01-05 16:58:20 +0300

    add: make root return endpoints

--- src/main/java/com/jester/backendserver/controller/RootController.java
+++ src/main/java/com/jester/backendserver/controller/RootController.java
@@ -1,14 +1,40 @@
 package com.jester.backendserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
+import org.springframework.boot.actuate.endpoint.web.Link;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpointDiscoverer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class RootController {
+    @Autowired
+    private WebEndpointDiscoverer webEndpointDiscoverer;
+    @Autowired
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
-    @GetMapping
-    public String status() {
-        return "Welcome to the Jester API!";
+//    @GetMapping
+//    public String status() {
+//        return "Welcome to the Jester API!";
+//    }
+
+    @GetMapping()
+    public Map<String, List<String>> getAllEndpoints() {
+        return requestMappingHandlerMapping.getHandlerMethods()
+                .entrySet()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        entry -> entry.getValue().getBeanType().getName(),
+                        Collectors.mapping(entry -> entry.getKey().toString(), Collectors.toList())
+                ));
     }
-    // TODO: Return a string that has information about the server api and endpoints.
 }
